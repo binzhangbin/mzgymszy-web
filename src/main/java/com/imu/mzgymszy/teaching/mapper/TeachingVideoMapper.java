e675dee1ee48b972c218962e99f7a2465931ef41
jiaxj <abcjiaxuejun@163.com>
2017-10-23 15:24:47 +0800

    增加教学视频Mapper java类及映射文件

---
+++ src/main/java/com/imu/mzgymszy/teaching/mapper/TeachingVideoMapper.java
@@ -0,0 +1,11 @@
+package com.imu.mzgymszy.teaching.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.imu.mzgymszy.teaching.model.TeachingVideoEntity;
+
+@Repository
+public interface TeachingVideoMapper extends BaseMapper<TeachingVideoEntity> {
+
+}
