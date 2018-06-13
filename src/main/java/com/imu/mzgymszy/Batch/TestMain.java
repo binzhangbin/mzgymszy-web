7daf02dea544730ac9c7af1a8f0a5371e9aec01f
jiaxj <abcjiaxuejun@163.com>
2018-06-13 11:40:52 +0800

    测试其它

---
+++ src/main/java/com/imu/mzgymszy/Batch/TestMain.java
@@ -0,0 +1,10 @@
+package com.imu.mzgymszy.Batch;
+
+
+public class TestMain {
+	public static void main(String[] args) {
+		
+		String str = "ᠡᠬᠢ ᠪᠠᠶᠠᠯᠢᠭ ᠤᠨ ᠬᠥᠮᠥᠷᠭᠡ ᠣᠷᠣᠬᠤ ᠠᠮᠠᠰᠠᠷ ";
+		System.out.println(str);
+	}
+}
